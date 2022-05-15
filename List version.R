21c46650c985b8c6c72548e465d276334c77c503
Qile <43093685+GTHB317@users.noreply.github.com>
2022-05-15 22:11:06 +0200

    Update List version.R

--- List version.R
+++ List version.R
@@ -1,6 +1,6 @@
 #This version takes in lists that must be ordered as such:
 #Area, x cord of centroid location, y cord of centroid location, hexadecimal color, label, and radius
-#Am not too sure yet if a list of lists in R is equivalent to a doubly linked list.
+#Am not too sure yet if a list of lists in R is equivalent to a doubly linked list. I am awaiting some help about how to handle this in R, hopefully.
 
 #load packages
 library(ggplot2)
@@ -44,7 +44,7 @@ ColorC <- '#0000FF'
 LabelC <- "Qile's cluster"
 RadiusC <- 3
 
-#example list that will be used for this script. 
+#example list that will be used later for this script. 
 V <- list(ClusterA, ClusterB, ClusterC)
 
 #radius from area  
@@ -57,6 +57,12 @@ Rfa<-function(a){
 
 #the doubly linked list structure doesn't really exist in R, but  I wonder whether or not a list of ordered lists works as well? Also, is it a circular linked list or a linear one?
 
+init_boundary <- function(a){
+  for (i in 1:length(a)-1){
+    message("Work in progress")
+  }
+}
+
 #original julia code:
 #function init_boundary(a::Array{Circle,1})
 #for i in 1:length(a)-1
@@ -67,6 +73,16 @@ Rfa<-function(a){
 #a[1].p = a[length(a)]
 #end
 
+fwd_dist <- function(c,d){
+  count <- 0
+  circ <- c
+  while(circ!=d){
+    count = count + 1
+    message("Work in progress")
+  }
+  count
+}
+
 #function fwd_dist(c::Circle, d::Circle) #number of "s"'s required to move fwd from c to d
 #count = 0
 #circ = c
@@ -76,6 +92,10 @@ Rfa<-function(a){
 #end
 #return count
 #end
+
+insert_circle <- function(c,d,e){
+  message("Work in progress")
+}
 
 #function insert_circle!(c::Circle,d::Circle,e::Circle)
 #if (c.s != d)||(d.p != c)
@@ -164,6 +184,16 @@ place_starting_three <- function(C1,C2,C3){
   C3[[3]] = C3[[3]] - centroid_y
 }
 
+closest <- function(c){
+  closest <- C
+  message("Work in progress")
+}
+
+closest_place <- function(c,d){
+  closest <- C
+  message("Work in progress")
+}
+
 do_intersect <- function(c,d){ #returns TRUE or FALSE
   sqrt((c[[2]]-d[[2]])^2 + (c[[3]]+d[[3]])^2) < c[[6]] + d[[6]] #could reformulate as discrepancy > threshold
 }
@@ -172,4 +202,79 @@ geod_dist <- function(Cm,Cn,C){ #fwd_dist doesnt exist yet
   min(fwd_dist(Cn,C), fwd_dist(C,Cm))
 }
 
-#not finished
+overlap_check <- function(Cm,Cn,C){
+  C_em <- Cm
+  C_en <- Cn
+  obstruct <- list()
+  message("Work in progress")
+}
+
+circle_layout <- function(input_rad_vec, order = TRUE, try_place = TRUE){
+  if (Order){
+    input_rad_vec = reverse(sort(input_rad_vec))
+  }
+
+  message("work in progress")
+}
+
+#Original Julia code below
+#function circle_layout(input_rad_vec::Array{Float64,1}; order = true, try_place = true)
+    #if order
+        #input_rad_vec = reverse(sort(input_rad_vec))
+    #end
+
+    # Initialise the circles with radii (not areas) as specified in input_rad_vec, and no boundary relations.
+    #circles = [Circle("Circle_$(i)", 0.0, 0.0, input_rad_vec[i],nothing,nothing) for i in 1:length(input_rad_vec)]
+
+    #Taking care of "degenerate" cases when there are one or two circles
+
+    #if length(circles) == 1
+        #return [[c.x for c in circles],[c.y for c in circles],[c.rad for c in circles]]
+    #elseif length(circles) == 2
+        #circles[1].x = - circles[1].rad
+        #circles[2].x = circles[2].rad
+        #return [[c.x for c in circles],[c.y for c in circles],[c.rad for c in circles]]
+    #end
+
+    # Place the first three circles to be mutually tangent, with centroid the origin.
+    #place_starting_three(circles[1], circles[2], circles[3])
+
+    # Initialise the boundary
+    #init_boundary(circles[1:3])
+
+    #= for i in 1:length(circles)
+        #println(circles[i].name,":",circles[i].rad)
+    #end =#
+    
+    #Loop through the remaining circles, fitting them
+    #j = 4
+    #while j <= length(circles)
+        # Initial attempt to fit Circle_j
+        #if try_place
+            #cl = closest_place(circles[j-1], circles[j])
+        #else
+            #cl = closest(circles[j-1])
+        #end
+        #fit_tang_circle!(cl, cl.s, circles[j])
+
+        # Check for overlaps and update, refit and recheck until "clear"
+        #check = overlap_check(cl, cl.s, circles[j])
+        #if check == "clear"
+            #insert_circle!(cl, cl.s, circles[j])
+            #j += 1
+        #else
+            #while check != "clear"
+                #Cm = check[1]
+                #Cn = check[2]
+                #fwd_remove!(Cm,Cn)
+                #fit_tang_circle!(Cm,Cn,circles[j])
+                #check = overlap_check(Cm,Cn,circles[j])
+                #if check == "clear"
+                    #insert_circle!(Cm,Cn, circles[j])
+                    #j += 1
+                #end
+            #end
+        #end
+    #end
+    #return [[c.x for c in circles],[c.y for c in circles],[c.rad for c in circles] ]
+#end
